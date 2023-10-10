1b758ca2579a118e44fe0355d026d9472da73b23
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-10-10 19:53:36 +0500

    Learned Hoisting & datatypes.

---
+++ index.js
@@ -0,0 +1,18 @@
+// 1. Hoisting
+
+// console.log(a);
+// var a=5;
+
+// 2. Data-Types in JS (reference vs primitive)
+
+// let a=[10,11];
+// b=a;
+// b.pop();
+
+// console.log(b);
+// console.log(a);
+// Reason: a is an array which is a reference datatype so its value is refering to b.
+// i.e a=[10,11]=b
+
+// 3. 
+
